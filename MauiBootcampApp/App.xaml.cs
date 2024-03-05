24f2e0b40f2d74b451f107e923c2377544ea26cb
Keith Stanley <iamstan13y@gmail.com>
2024-03-05 15:40:49 +0200

    Add project files.

---
+++ MauiBootcampApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiBootcampApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiBootcampApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiBootcampApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
